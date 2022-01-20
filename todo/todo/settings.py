0e1f1ca32754e2fc7d6d51186f65c831954685cd
Nijat <nicatrecebli1996@gmail.com>
2022-01-20 21:06:09 +0400

    Debug mode added

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_root")]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CSRF_TRUSTED_ORIGINS = ["https://todoapp-pvlnj4az7q-lm.a.run.app"]
 
